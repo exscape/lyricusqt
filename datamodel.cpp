423dae9dddfa5066a9f21e33f4e48982cea84ba3
Thomas Backman <serenity@exscape.org>
2016-05-29 21:51:11 +0200

    Implement setLyricsForFile for MP3

--- datamodel.cpp
+++ datamodel.cpp
@@ -172,6 +172,68 @@ QString DataModel::lyricsForFile(const QString &path) {
     }
 }
 
+bool DataModel::setLyricsForFile(const QString &path, const QString &lyrics) {
+    if (path.endsWith(".mp3")) {
+        TagLib::MPEG::File mpf(QFile::encodeName(path).constData());
+        if (!mpf.isOpen() || !mpf.isValid())
+            return false;
+
+        auto *id3v2tag = mpf.ID3v2Tag();
+        if (!id3v2tag)
+            return false;
+
+        TagLib::ID3v2::FrameList frames = id3v2tag->frameListMap()["USLT"];
+        if (frames.isEmpty()) {
+            qDebug() << "No USLT frame found; creating one";
+            auto *frame = new TagLib::ID3v2::UnsynchronizedLyricsFrame;
+            frame->setLanguage("eng");
+            id3v2tag->addFrame(frame);
+            frames = id3v2tag->frameListMap()["USLT"];
+        }
+
+        if (frames.isEmpty()) {
+            qWarning() << "Unable to read/create USLT frame for" << path;
+            return false;
+        }
+
+        auto *frame = dynamic_cast<TagLib::ID3v2::UnsynchronizedLyricsFrame*>(frames.front());
+        if (!frame)
+            return false;
+
+        frame->setText(QStringToTString(lyrics));
+
+        bool success = mpf.save();
+        if (!success)
+            qWarning() << "Unable to save lyrics to" << path;
+
+        return success;
+
+    }
+    else if (path.endsWith(".m4a")) {
+
+        //
+        // TODO: Implement M4A tag writing
+        //
+
+        /*
+        TagLib::MP4::File file(QFile::encodeName(path).constData());
+        if (!file.isOpen() || !file.isValid())
+            return {};
+
+        TagLib::MP4::Item item = file.tag()->itemListMap()["\xa9lyr"];
+        if (!item.isValid())
+
+        TagLib::StringList strings = item.toStringList();
+        if (!strings.isEmpty()) {
+            return strings.front().toCString(true);
+        }
+        else
+            return {};
+        return false;
+        */
+    }
+}
+
 void DataModel::updateIndex() {
     QSqlQuery query("DELETE FROM data");
     id = 1;
--- datamodel.h
+++ datamodel.h
@@ -19,6 +19,7 @@ public:
 private:
     int id = 1;
     QString lyricsForFile(const QString &path);
+    bool setLyricsForFile(const QString &path, const QString &lyrics);
     QSqlDatabase db;
 };
 
