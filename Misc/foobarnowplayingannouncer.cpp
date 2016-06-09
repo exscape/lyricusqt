2160bffef32126407e79599fa4e5f12e6f208d3d
Thomas Backman <serenity@exscape.org>
2016-06-09 19:30:27 +0200

    Loop and retry etc. in the foobar interface

--- Misc/foobarnowplayingannouncer.cpp
+++ Misc/foobarnowplayingannouncer.cpp
@@ -1,5 +1,6 @@
 #include "foobarnowplayingannouncer.h"
 #include <QDebug>
+#include <QThread>
 
 // TODO: REMOVE, DEBUGGING ONLY
 #include <QWaitCondition>
@@ -18,68 +19,62 @@ FoobarNowPlayingAnnouncer::FoobarNowPlayingAnnouncer(QObject *parent) : QObject(
 }
 
 void FoobarNowPlayingAnnouncer::run() {
-    // TODO
-    // TODO: error handling, retrying etc!
-    // TODO
-
-    HANDLE hPipe = CreateFile(TEXT("\\\\.\\pipe\\foo_simpleserver_announce"), GENERIC_READ | FILE_WRITE_ATTRIBUTES /* NO GENERIC_WRITE */, 0, NULL, OPEN_EXISTING, 0, NULL);
-    if (hPipe == INVALID_HANDLE_VALUE) {
-        qWarning() << "Unable to connect to foo_simpleserver_announce pipe";
-        return;
-    }
-
-    DWORD mode = PIPE_READMODE_MESSAGE;
-    if (!SetNamedPipeHandleState(hPipe, &mode, NULL, NULL)) {
-        qWarning() << "Unable to set PIPE_READMODE_MESSAGE for foo_simpleserver_announce pipe";
-        return;
-    }
-
     char *responseData = (char *)HeapAlloc(GetProcessHeap(), 0, BUFSIZE);
     if (!responseData) {
-        CloseHandle(hPipe);
         qWarning() << "Unable to allocate memory for foo_simpleserver_announce response data";
         return;
     }
 
+    bool initial_run = true;
+
+    // Loop retrying to connect to the pipe, in case it fails (or disconnects after initially succeeding)
     while (true) {
-        DWORD bytes_read = 0;
-        bool success = ReadFile(hPipe, responseData, BUFSIZE, &bytes_read, NULL);
+restart_loop:
+        if (!initial_run) {
+            QThread::sleep(4);
+        }
+        else
+            initial_run = false;
 
-        if (!success) {
-            qWarning() << "ReadFile failed; GetLastError() ==" << GetLastError();
-            CloseHandle(hPipe);
-            HeapFree(GetProcessHeap(), 0, responseData);
-            return;
+        HANDLE hPipe = CreateFile(TEXT("\\\\.\\pipe\\foo_simpleserver_announce"), GENERIC_READ | FILE_WRITE_ATTRIBUTES, 0, NULL, OPEN_EXISTING, 0, NULL);
+        if (hPipe == INVALID_HANDLE_VALUE) {
+            continue;
         }
 
-        if (bytes_read != sizeof(struct songInfo)) {
-            qWarning() << "foo_simpleserver_announce read failed: unexpected data size" << bytes_read << "bytes";
+        DWORD mode = PIPE_READMODE_MESSAGE;
+        if (!SetNamedPipeHandleState(hPipe, &mode, NULL, NULL)) {
+            qWarning() << "Unable to set PIPE_READMODE_MESSAGE for foo_simpleserver_announce pipe";
             CloseHandle(hPipe);
-            HeapFree(GetProcessHeap(), 0, responseData);
-            return;
+            continue;
         }
 
+        // Loop while connected to this pipe
+        while (true) {
+            DWORD bytes_read = 0;
+            bool success = ReadFile(hPipe, responseData, BUFSIZE, &bytes_read, NULL);
 
-        struct songInfo *info = reinterpret_cast<struct songInfo *>(responseData);
+            if (!success) {
+                qWarning() << "foo_simpleserver_announce ReadFile failed; GetLastError() ==" << GetLastError();
+                CloseHandle(hPipe);
+                goto restart_loop; // Weee!
+            }
 
-        qDebug() << "New song:" << info->artist << "-" << info->title;
+            if (bytes_read != sizeof(struct songInfo)) {
+                qWarning() << "foo_simpleserver_announce read failed: unexpected data size" << bytes_read << "bytes";
+                CloseHandle(hPipe);
+                goto restart_loop; // Weee!
+            }
+
+            struct songInfo *info = reinterpret_cast<struct songInfo *>(responseData);
+
+            QString artist = QString::fromUtf8(info->artist);
+            QString title = QString::fromUtf8(info->title);
+
+            if (artist.length() > 0 && title.length() > 0)
+                emit newTrack(artist, title);
+        }
     }
 
-//  FlushFileBuffers(hPipe);
+    // This won't ever be reached... oh well.
     HeapFree(GetProcessHeap(), 0, responseData);
-
-    /*
-    int i = 0;
-    while (true) {
-        qDebug() << "FoobarNowPlayingAnnouncer still here...";
-        QMutex mutex;
-        mutex.lock();
-        QWaitCondition waitCondition;
-        waitCondition.wait(&mutex, 1000);
-        mutex.unlock();
-
-        if (i++ % 5 == 0)
-            emit newTrack("Some artist", QString("Some track #%1").arg(i));
-    }
-    */
 }
